6a1a40b70718ea59272892934730f189ad9fbcda
curtbrink <crtbrnk+github@gmail.com>
2025-08-20 23:23:22 -0400

    Add new endpoint to test a PR build

--- CorkbanPrintsrv/Program.cs
+++ CorkbanPrintsrv/Program.cs
@@ -17,6 +17,12 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+app.MapGet("/test", () =>
+{
+    var foo = "bar";
+    return new { foo };
+}).WithName("GetTest");
+
 app.MapGet("/weatherforecast", () =>
     {
         var forecast = Enumerable.Range(1, 5).Select(index =>
